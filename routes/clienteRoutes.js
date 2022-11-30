d8c1de196e3c02a9a6de3dfcddbc181f952ee776
Ronie <menezesronie@gmail.com>
2022-11-30 00:49:47 -0300

    Conclusão rota http get

--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -19,8 +19,29 @@ router.post('/cliente', async (req, res) => {
             res.status(201).json({message: 'Cliente Cadastrado com Sucesso!!!!'})
      res.status(500).send()
      } catch (error) {
-        res.status.apply(500).json({error: error})
-        
+        res.status.apply(500).json({error: error})  
      }
 })
+router.get('/cliente/:id?', async (req, res) => {
+    const id = req.params.id
+    let resultado = {}
+    if(id)
+        resultado = {_id: id}
+
+    try {
+        const cliente = await Cliente.find(resultado)
+        if(cliente)
+            res.status(200).json(cliente)
+        else
+            res.status(404).json({message: 'Cliente Não Encontrado'})
+
+    } catch (error) {
+        res.status(500).json({error: error})
+        
+    }
+})
+
+
+
+
 module.exports = router
